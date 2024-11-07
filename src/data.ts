a1012ec9924afeae78016e0232616cf91dec33ac
n4n5 <its.just.n4n5@gmail.com>
2024-11-07 11:41:49 +0100

    update && add avatar & types

--- src/data.ts
+++ src/data.ts
@@ -1,14 +1,24 @@
 import { readFileSync } from "fs";
 import { parse } from "csv/sync";
+import type { ActivityType } from "./types";
 
 const options = {
     columns: true,
     skip_empty_lines: true,
 };
 
-export const activities = parse(readFileSync("export/activities.csv", "utf-8"), options);
+export const activities: ActivityType[] = parse(readFileSync("export/activities.csv", "utf-8"), options);
 
-export const user = parse(readFileSync("export/profile.csv", "utf-8"), options)[0];
+const getUser = () => {
+    const csvUser = parse(readFileSync("export/profile.csv", "utf-8"), options)[0];
+    return {
+        ...csvUser,
+        name: csvUser["First Name"],
+        avatar: `https://www.gravatar.com/avatar/${csvUser["Email Address Sha"]}`,
+    };
+};
+
+export const user = getUser();
 
 export const followers = parse(readFileSync("export/followers.csv", "utf-8"), options);
 
